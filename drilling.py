db7da6aa25253c49dbfc03b1f28ad6a73009499e
Nurgle <axilles90@gmail.com>
2018-12-09 20:11:13 +0300

    Добавил запас толщины для большей надёжности

--- drilling.py
+++ drilling.py
@@ -2,15 +2,18 @@
 
 print("Пожалуйста, введите размеры элементов в миллиметрах")
 
-wall_thickness = input("Ввведите толщину стены: ")
+wall_thickness = input("Введите толщину стены: ")
 
 anker_length = input("Введите длину анкера: ")
 
 bar_thickness = input("Введите толщину бруса: ")
+
+# запас толщины стенки в миллиметрах
+reserve = 30
 
 # меняем входные данные на целочисленые значения
 wall_thickness = int(wall_thickness)
 anker_length = int(anker_length)
 bar_thickness = int(bar_thickness)
 
-print("\n Variable result:" , "good" if (wall_thickness > (anker_length - bar_thickness)) else "horribly")
+print("\n Variable result:" , "good" if (wall_thickness > (anker_length - bar_thickness + reserve)) else "horribly")
