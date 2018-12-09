dd885ca6e6a6949cd86a9c1f6a030437a3850f46
Nurgle <axilles90@gmail.com>
2018-12-09 19:28:02 +0300

    расчёт необоходимой длины крепёжного элемента

--- drilling.py
+++ drilling.py
@@ -1,1 +1,12 @@
-1
+# расчёт необходимой длины крепежного элемента
+
+wall_thickness = input("Ввведите толщину стены: ")
+
+anker_length = input("Введите длину анкера: ")
+
+bar_thickness = input("Введите толщину бруса: ")
+
+wall_thickness = int(wall_thickness)
+anker_length = int(anker_length)
+bar_thickness = int(bar_thickness)
+print("\n Variable result:" , "good" if (wall_thickness > (anker_length - bar_thickness)) else "horribly")
